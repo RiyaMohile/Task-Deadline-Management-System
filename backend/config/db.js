5585cb06ac6fd166a3294f1b2388dba89d72692b
Riya Mohile <riyamohile812@gmail.com>
2025-12-29 11:43:08 +0530

    change url to connect db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ const createDefaultUsers = require("../seed/defaultUsers");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI).then(res=>{
 
-    // ✅ CREATE DEFAULT USERS AFTER DB CONNECT
+      console.log("MongoDB Connected");
+    })
+
     await createDefaultUsers();
   } catch (err) {
     console.error(err);
